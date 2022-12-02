50797623cd81eb91882128ab1e8144c9edb6d772
Denis Polyakov <p-ov.da@ya.ru>
2022-12-02 10:44:46 +0300

    Add CLI options

---
+++ listen-minechat.py
@@ -0,0 +1,43 @@
+import argparse
+import asyncio
+import datetime
+
+import aiofiles
+from environs import Env
+
+
+async def save_message(message, chat_history_filename):
+    current_time = datetime.datetime.now()
+    message = f"{current_time.strftime('[%d.%m.%y %H:%M]')} {message}"
+    print(message)
+    async with aiofiles.open(chat_history_filename, mode='a') as chat_file:
+        await chat_file.write(message)
+
+
+async def tcp_reader(tcp_config):
+    reader, writer = await asyncio.open_connection(
+        tcp_config['host'], tcp_config['port'])
+
+    while not reader.at_eof():
+        message = await reader.readline()
+        await save_message(message.decode(), tcp_config['history'])
+
+    print('Close the connection')
+    writer.close()
+
+
+if __name__ == '__main__':
+    tcp_config = {}
+    env = Env()
+    env.read_env()
+    tcp_config['host'] = env.str('HOST', 'minechat.dvmn.org')
+    tcp_config['port'] = env.int('PORT', 5000)
+    tcp_config['history'] = env.str('HISTORY', 'chat_history.txt')
+    parser = argparse.ArgumentParser(description='Communicate with TCP chat')
+    parser.add_argument('--host', help='host of chat')
+    parser.add_argument('--port', type=int, help='port of chat')
+    parser.add_argument('--history', help='path to file with chat history')
+    args = parser.parse_args()
+    for key, value in vars(args).items():
+        tcp_config[key] = value or tcp_config[key]
+    asyncio.run(tcp_reader(tcp_config))
