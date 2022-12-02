0c06092c853d275ec367497e9e3c06427e633e7d
Denis Polyakov <p-ov.da@ya.ru>
2022-12-02 23:07:55 +0300

    Add check for broken token

--- send-message.py
+++ send-message.py
@@ -1,5 +1,6 @@
 import argparse
 import asyncio
+import json
 import logging
 
 from environs import Env
@@ -7,26 +8,39 @@ from environs import Env
 logger = logging.getLogger(__name__)
 
 
+async def register(reader: asyncio.StreamReader, writer: asyncio.StreamWriter):
+    writer.write("\n".encode())
+    await writer.drain()
+
+    response = await reader.readline()
+    logger.info(response.decode())
+
+    username = input('У вас отсутствует токен. Введите ваше имя: ')
+    writer.write(f"{username}\n\n".encode())
+    logger.info(f'SEND: {username}')
+    await writer.drain()
+
+
 async def tcp_writer(tcp_config):
     reader, writer = await asyncio.open_connection(
         tcp_config['host'], tcp_config['port'])
 
-    reponse = await reader.readline()
-    logger.info(reponse.decode())
+    response = await reader.readline()
+    logger.info(response.decode())
     if tcp_config['token']:
         writer.write(f"{tcp_config['token']}\n".encode())
         await writer.drain()
     else:
-        writer.write("\n".encode())
-        await writer.drain()
-        reponse = await reader.readline()
-        logger.info(reponse.decode())
-        username = input('У вас отсутствует токен. Введите ваше имя: ')
-        writer.write(f"{username}\n\n".encode())
-        logger.info(f'SEND: {username}')
-        await writer.drain()
-    reponse = await reader.readline()
-    logger.info(reponse.decode())
+        await register(reader, writer)
+    response = await reader.readline()
+    decoded_response = json.loads(response)
+    logger.info(response)
+    if not decoded_response:
+        logger.info('Неправильный токен. Отправляем на регистрацию ...')
+        reader, writer = await asyncio.open_connection(
+            tcp_config['host'], tcp_config['port'])
+        await register(reader, writer)
+
     while True:
         message = input('Введите ваше сообщение: ')
         writer.write(f"{message}\n\n".encode())
@@ -43,6 +57,7 @@ if __name__ == '__main__':
         format='%(levelname)s:%(name)s:%(message)s',
         level=logging.INFO
     )
+    logging.getLogger().addHandler(logging.StreamHandler())
     tcp_config = {}
     env = Env()
     env.read_env()
