655d70ddbcee30607dac7c4808bd9e92c98c2a1b
Denis Polyakov <p-ov.da@ya.ru>
2022-12-07 15:53:32 +0300

    Fix open_connection func

--- tcp_tools.py
+++ tcp_tools.py
@@ -7,10 +7,9 @@ logger = logging.getLogger(__name__)
 
 @asynccontextmanager
 async def open_connection(host, port):
-    conn = await asyncio.open_connection(host, port)
-    reader, writer = conn
+    reader, writer = await asyncio.open_connection(host, port)
     try:
-        yield conn
+        yield reader, writer
     finally:
         logger.info('Close the connection')
         writer.close()
